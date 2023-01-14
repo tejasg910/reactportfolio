5bf0074b83c2ebb8432e90baf4e0f33f43108a4c
Tejas <tejasgiri910@gmail.com>
2023-01-14 17:22:23 +0530

    added the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,61 +5,122 @@ const Projects = () => {
         <div className='bg-indigo-800'>
             <h2 className='text-4xl font-bold text-white mx-4 '>Projects</h2>
             <div className='flex flex-col lg:flex-row  items-center'>
-
+                <div className='w-ful lg:w-1/2 m-4'>
+                    <div>
+                        <img className='h-80' src="universitywebsite.PNG" alt="" />
+                    </div>
+                    <div class="flex justify-around  m-2">
+                        <a href="https://university-frontend.vercel.app/" target={"_blank"}>    <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/university-frontend" target={"_blank"}>  <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        >Source Code</button></a>
+                    </div>
+                </div>
+                <div className='w-ful lg:w-1/2 m-4'>
+                    <div>
+                        <img className='h-80' src="movierating.PNG" alt="" />
+                    </div>
+                    <div class="flex justify-around  m-2">
+                        <a href="https://movieratingwebsite-el5dv2nj4-tejasg910.vercel.app/" target={"_blank"}>    <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/movieratingwebsite" target={"_blank"}>  <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        >Source Code</button></a>
+                    </div>
+                </div>
                 <div className='w-ful lg:w-1/2 m-4'>
                     <div>
                         <img className='h-80' src="bookabook.PNG" alt="" />
                     </div>
-                    <div class="flex justify-center m-2">
-                        <button
+                    <div class="flex justify-around  m-2">
+                        <a href="https://bookabook.netlify.app/" target={"_blank"}>    <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/rent-a-book" target={"_blank"}>  <button
                             type="button"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
                             class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                        >Details</button>
+                        >Source Code</button></a>
                     </div>
                 </div>
+
+
+            </div>
+            <div className='flex flex-col lg:flex-row  items-center'>
                 <div className='w-ful lg:w-1/2 m-4'>
                     <div>
                         <img className='h-80' src="university.PNG" alt="" />
                     </div>
-                    <div class="flex justify-center m-2">
-                        <button
+                    <div class="flex justify-around  m-2">
+                        <a href="https://universitywebsitejoin.netlify.app/">   <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/college-website" target={"_blank"}>  <button
                             type="button"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
                             class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                        >Details</button>
+                        >Source Code</button></a>
                     </div>
                 </div>
-
-            </div>
-            <div className='flex flex-col lg:flex-row  items-center'>
-
                 <div className='w-ful object-fill lg:w-1/2 '>
                     <div>
                         <img className='h-80 w-[90%] m-auto' src="weather.PNG" alt="" />
                     </div>
-                    <div class="flex justify-center m-2">
-                        <button
+                    <div class="flex justify-around  m-2">
+                        <a href="https://weatherappbytejas.netlify.app/" target={"_blank"}>   <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/weather-website" target={"_blank"}>  <button
                             type="button"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
                             class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                        >Details</button>
+                        >Source Code</button></a>
                     </div>
                 </div>
                 <div className='w-ful lg:w-1/2 m-4'>
                     <div>
                         <img className='h-80' src="shoping.PNG" alt="" />
                     </div>
-                    <div class="flex justify-center m-2">
-                        <button
+                    <div class="flex justify-around  m-2">
+                        <a href="https://shopkaro1.netlify.app/" target={"_blank"}>   <button
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            class="inline-block px-6 py-2.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-100 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-200 active:shadow-lg transition duration-150 ease-in-out"
+                        >Preview</button></a>
+                        <a href="https://github.com/tejasg910/Ladies-shopping" target={"_blank"}>  <button
                             type="button"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
                             class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                        >Details</button>
+                        >Source Code</button></a>
                     </div>
                 </div>
 
